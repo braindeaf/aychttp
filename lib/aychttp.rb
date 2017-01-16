8b14ea4437db84e1214240bb76e32a157c6920a5
Rob Lacey <contact@robl.me>
2017-01-16 12:27:48 +0000

    AychTTP#uri_for

--- lib/aychttp.rb
+++ lib/aychttp.rb
@@ -4,4 +4,18 @@ require 'aychttp/version'
 
 module AychTTP
   extend Request::Methods
+
+  module Methods
+    def uri_for(args)
+      host, port, path = [args.delete(:host), args.delete(:port), args.delete(:path)]
+      builder = args.delete(:protocol) == 'https' ? URI::HTTPS : URI::HTTP
+
+      uri = builder.build host: host, port: port, path: path
+      unless args.empty?
+        uri.query = args.reject { |_k, v| v.nil? }.sort_by { |k, _v| k.to_s }.map { |k, v| "#{k}=#{v.to_s}" }.join('&')
+      end
+      uri.to_s
+    end
+  end
+  extend Methods
 end
